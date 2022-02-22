63d2ac7cd27005447fa9e65842dd5dca55401397
Martin Reale / Espulito <espulito@gmail.com>
2022-02-22 16:36:05 -0300

    Abreviando funciones con parámetros.

--- main.js
+++ main.js
@@ -6,129 +6,18 @@ function sumarScore() {
   score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
 }
 
-function obrero() {
-  if (puntaje >= 10) {
-    puntaje -= 10;
-    setInterval(() => {
-      puntaje += 0.1;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let obrerobox = document.getElementById("obrerobox");
-    obrerobox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
-  }
-}
+// Trabajos
 
-function panadero() {
-  if (puntaje >= 25) {
-    puntaje -= 25;
+function trabajos(cuantoCuesta, cuantoDa, trabajoBox) {
+  if (puntaje >= cuantoCuesta) {
+    puntaje -= cuantoCuesta;
     setInterval(() => {
-      puntaje += 0.3;
+      puntaje += cuantoDa;
       score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
     }, 1000);
-    let panaderobox = document.getElementById("panaderobox");
-    panaderobox.classList.add("desaparecer");
+    document.getElementById(trabajoBox).classList.add("desaparecer");
   } else {
-    alert("No se pudo...");
-  }
-}
-
-function verdulero() {
-  if (puntaje >= 50) {
-    puntaje -= 50;
-    setInterval(() => {
-      puntaje += 0.5;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let verdulerobox = document.getElementById("verdulerobox");
-    verdulerobox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
-  }
-}
-
-function kiosquero() {
-  if (puntaje >= 100) {
-    puntaje -= 100;
-    setInterval(() => {
-      puntaje += 1;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let kiosquerobox = document.getElementById("kiosquerobox");
-    kiosquerobox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
-  }
-}
-
-function armero() {
-  if (puntaje >= 250) {
-    puntaje -= 250;
-    setInterval(() => {
-      puntaje += 3;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let armerobox = document.getElementById("armerobox");
-    armerobox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
-  }
-}
-
-function abogado() {
-  if (puntaje >= 500) {
-    puntaje -= 500;
-    setInterval(() => {
-      puntaje += 5;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let abogadobox = document.getElementById("abogadobox");
-    abogadobox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
-  }
-}
-
-function contador() {
-  if (puntaje >= 1000) {
-    puntaje -= 1000;
-    setInterval(() => {
-      puntaje += 10;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let contadorbox = document.getElementById("contadorbox");
-    contadorbox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
-  }
-}
-
-function inversionista() {
-  if (puntaje >= 5000) {
-    puntaje -= 5000;
-    setInterval(() => {
-      puntaje += 50;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let inversionistabox = document.getElementById("inversionistabox");
-    inversionistabox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
-  }
-}
-
-function empresario() {
-  if (puntaje >= 10000) {
-    puntaje -= 10000;
-    setInterval(() => {
-      puntaje += 100;
-      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
-    }, 1000);
-    let empresariobox = document.getElementById("empresariobox");
-    empresariobox.classList.add("desaparecer");
-  } else {
-    alert("No se pudo...");
+    alert("Tu puntaje actual es menor a " + cuantoCuesta + ".");
   }
 }
 
