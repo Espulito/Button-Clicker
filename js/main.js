6eab1ab55daf4f5ef3eff83e34b2ce093cc1c652
Martin Reale / Espulito <espulito@gmail.com>
2022-02-22 22:05:47 -0300

    Nueva organización de archivos e implementaciÃón de la librería Sweetalert

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const score = document.getElementById("score");
+var puntaje = 0;
+
+function sumarScore() {
+  puntaje += 1;
+  score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
+}
+
+// Trabajos
+
+function trabajos(cuantoCuesta, cuantoDa, trabajoBox) {
+  if (puntaje >= cuantoCuesta) {
+    puntaje -= cuantoCuesta;
+    setInterval(() => {
+      puntaje += cuantoDa;
+      score.innerHTML = "Puntuación: " + puntaje.toFixed(1);
+    }, 1000);
+    document.getElementById(trabajoBox).classList.add("desaparecer");
+  } else {
+    alert("Tu puntaje actual es menor a " + cuantoCuesta + ".");
+  }
+}
