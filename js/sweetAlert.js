10ba276f6dbf6913debde24cf206a5321d1d97ea
Martin Reale / Espulito <espulito@gmail.com>
2022-02-23 01:01:31 -0300

    Implementación de una ventana modal para comprender el funcionamiento del juego.

---
+++ js/sweetAlert.js
@@ -0,0 +1,7 @@
+Swal.fire({
+  icon: "question",
+  title: "¿Sabes como jugar?",
+  text: 'El "Juego" es muy simple y fácil de comprender, únicamente consiste en presionar el botón para conseguir puntos y posteriormente comprar trabajos para conseguir estos mismos automáticamente.',
+  showConfirmButton: false,
+  showCloseButton: true,
+});
